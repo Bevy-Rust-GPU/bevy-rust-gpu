d17ae0f1d92209c10e2bbcfbb8600e8704c173d7
Shfty <Shfty@users.noreply.github.com>
2023-03-08 07:47:52 +0000

    deploy: 8d9f574f6abf19ad0f52821fafa8a4658a086bce

--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bevy_rust_gpu":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.152/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bevy_rust_gpu/shader_meta/struct.ModuleMeta.html\" title=\"struct bevy_rust_gpu::shader_meta::ModuleMeta\">ModuleMeta</a>"]]
+"bevy_rust_gpu":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.153/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bevy_rust_gpu/shader_meta/struct.ModuleMeta.html\" title=\"struct bevy_rust_gpu::shader_meta::ModuleMeta\">ModuleMeta</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
