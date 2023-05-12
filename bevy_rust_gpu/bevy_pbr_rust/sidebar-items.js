4a08b7fb1a127cf62239301f4e0dfebe3846e577
Shfty <Shfty@users.noreply.github.com>
2023-05-12 10:22:32 +0000

    deploy: 2d9b667d769f49598f28480cc2704890341837b3

--- bevy_rust_gpu/bevy_pbr_rust/sidebar-items.js
+++ bevy_rust_gpu/bevy_pbr_rust/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["MeshFragment","`bevy_rust_gpu::mesh::entry_points::fragment`"],["MeshVertex","`bevy_rust_gpu::mesh::entry_points::vertex`"],["PbrFragment","`bevy_rust_gpu::pbr::entry_points::fragment`"]]};
+window.SIDEBAR_ITEMS = {"enum":["MeshFragment","MeshVertex","PbrFragment"]};
--- implementors/bevy_reflect/type_uuid/trait.TypeUuid.js
+++ implementors/bevy_reflect/type_uuid/trait.TypeUuid.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bevy_rust_gpu":[["impl&lt;M:&nbsp;TypeUuid&gt; TypeUuid for <a class=\"struct\" href=\"bevy_rust_gpu/prelude/struct.RustGpu.html\" title=\"struct bevy_rust_gpu::prelude::RustGpu\">RustGpu</a>&lt;M&gt;"]]
+"bevy_rust_gpu":[["impl&lt;M: TypeUuid&gt; TypeUuid for <a class=\"struct\" href=\"bevy_rust_gpu/prelude/struct.RustGpu.html\" title=\"struct bevy_rust_gpu::prelude::RustGpu\">RustGpu</a>&lt;M&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
